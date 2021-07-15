c93210deb2a42af9b8a566cf7a408b4a6f40a6ea
Julia Ruban <63366643+July-1390@users.noreply.github.com>
2021-07-15 22:01:32 +0200

    add votes icons

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,20 +1,36 @@
 import React from "react";
-import { FiThumbsUp } from "react-icons/fi";
+// import { FiThumbsUp } from "react-icons/fi";
+import { MdThumbUp, MdThumbDown } from "react-icons/md";
 import "./Votes.scss";
 
 const Votes = () => {
   return (
-    <div className="vote-container">
-      <div className="vote-wrapper">
-        <h3 className="percentage">35%</h3>
-        <div className="icon-like">
-          <FiThumbsUp />
-        </div>
-        <div className="count-votes">
-          <p>170 votes</p>
+    <>
+      <div className="rating">
+        <div className="rate-box">
+          <div className="rating-plus">
+            <MdThumbUp />
+          </div>
+          <div className="rating-count">
+            <p className="rate-num">+7</p>
+          </div>
+          <div className="rating-minus">
+            <MdThumbDown />
+          </div>
         </div>
       </div>
-    </div>
+    </>
+    // <div className="vote-container">
+    //   <div className="vote-wrapper">
+    //     <h3 className="percentage">35%</h3>
+    //     <div className="icon-like">
+    //       <FiThumbsUp />
+    //     </div>
+    //     <div className="count-votes">
+    //       <p>170 votes</p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 

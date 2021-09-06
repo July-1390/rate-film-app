a363279b3087310e6f6dc9748967fcdc5e75b6fd
Julia Ruban <63366643+July-1390@users.noreply.github.com>
2021-09-06 21:22:43 +0200

    add grid

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,67 +1,71 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {RiStarSFill} from 'react-icons/ri'
 
 import "./UserProfile.scss";
 
 const UserProfile = () => {
   return (
     <>
-      <div className="wrapper">
+      <div className="profile-container">
         <div className="profile-header-section">
-            <h1>User Profile Page</h1>
+            <h1 className='title'>User Profile Page</h1>
         </div>
-        <div className="row">
           <div className="left-column">
-            <div className="panel-widget">
-                <div className="text-center">
+            <div className="avatar-widget">
                   <img
                     alt="user-avatar"
                     src="https://images.fandango.com/cms/assets/a2f0b9d0-cf84-11eb-a507-3be98bb99253--actordefaulttmb.gif"
                   />
-                </div>
-                <h2 className="user-name">User Names</h2>
             </div>
+            <h2 className="user-name">User Names</h2>
           </div>
 
           <div className="right-column">
             <div className='nav-user-profile'>
-              <ul>
-                  <li>
+              <ul className='navigation'>
+                  <li className='first'>
                     <Link className="profile-links" to="/">
                       Movie Ratings
                     </Link>
                   </li>
-                  <li>
+                  <li className='second'>
                     <Link className="profile-links" to="/">
                       Profile
                     </Link>
                   </li>
-                  <li>
+                  <li className='third'>
                     <Link className="profile-links" to="/">
                       Manage account
                     </Link>
                   </li>
               </ul>
-              <div className="pd-20">
-                <div className="panel-body">
-                  <ul>
-                    <li className="rating-user-review">
-                      <section className="user-rating">
+            </div>
+                <div className="panel-list-films">
+                    <div className="one-film">
+                      
                         <div className="rating-poster">
                           <img
                             alt="poster"
                             src="https://resizing.flixster.com/5avIoBUmkdWfFcA8QOn0VM8BI28=/170x243/v2/https://resizing.flixster.com/xQQsLC7IJypTxyRXeOvnPwdEWwE=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzE0ZWFjM2Y1LTYzNTYtNGIwNS1iNGU2LTk0NTA2MGQ3NjY3NC53ZWJw"
                           />
                         </div>
-                        <section className="rating-review-top"></section>
-                      </section>
-                    </li>
-                  </ul>
+                          <div className='name-of-film'>
+                            <p className='rating-movie-title'>Drive</p>
+                          </div>
+                          <div className='rating-stars'>
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                            <RiStarSFill />
+                          </div>
+                        
+                    </div>
                 </div>
-              </div>
-            </div>
+              
           </div>
-        </div>
+        
       </div>
     </>
   );

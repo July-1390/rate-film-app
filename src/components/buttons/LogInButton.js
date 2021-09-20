8cb06754f85ca83cb5ed3c5447110d95aae4728b
Julia Ruban <63366643+July-1390@users.noreply.github.com>
2021-09-20 23:23:44 +0200

    create new direction for buttons and add some buttons into separate components

---
+++ src/components/buttons/LogInButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './LogInButton.scss'
+
+const LogInButton = ({setIsModalVisible}) => {
+
+    const handleShow = () => {setIsModalVisible(true)};
+
+    return (
+        <>
+            <button className="btn btn-primary" onClick={handleShow}>Log In</button>
+        </>
+    )
+
+}
+
+export default LogInButton;
